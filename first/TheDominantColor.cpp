233a60d5bf0cae7fa7561f48da3769202cb59040
DreamBlack <1104122155@qq.com>
2018-02-28 15:20:28 +0800

    1054. The Dominant Color (20)-map应用

--- first/TheDominantColor.cpp
+++ first/TheDominantColor.cpp
@@ -3,6 +3,11 @@
 using namespace std;
 map<int,int> pix;
 int main(){
+	/*
+	题目大意：选取主色调，就是M列N行的矩阵里面出现次数多余一半的那个数字。
+	解题思路：
+	由于题目对时间效率要求比较高。因此使用map(自带hash)，用值记录每种颜色出现的次数，同时保存出现次数最多的那次最后输出
+	*/
 	int m,n;
 	scanf("%d%d",&m,&n);
 	int max=-1,maxColor=-1;;
